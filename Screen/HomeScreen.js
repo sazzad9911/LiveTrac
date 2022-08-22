b3b25d631cf526e45394c3e7cfa4793863179f0f
Sazzad Hossain <sazzad15-2521@diu.edu.bd>
2022-08-22 19:05:49 +0600

    added android feature

--- Screen/HomeScreen.js
+++ Screen/HomeScreen.js
@@ -2,19 +2,17 @@ import React from 'react';
 import { View,Text , StatusBar} from 'react-native';
 import { ScrollView } from 'react-native';
 import Cart from '../Components/Cart';
-import { getFirestore,collection,query } from "firebase/firestore";
 import FoodItem from '../Data/FoodItem.json';
-import firebase from '../firebase';
 
 
 function HomeScreen(props) {
-    const db = getFirestore(firebase);
-    const citiesRef = collection(db, "Foods");
+   
+   
     const [Data, setData] = React.useState(null);
 
     React.useEffect(()=>{
-        const q = query(citiesRef);
-        console.log(q)
+        
+        
 
     },[Data])
     return (
