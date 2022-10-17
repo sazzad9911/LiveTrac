021211c6a922cbe5da0e67de43b617f07bd52ff1
Sazzad Hossain <sazzad15-2521@diu.edu.bd>
2022-10-17 14:41:57 +0600

    okk

--- Screen/HomeScreen.js
+++ Screen/HomeScreen.js
@@ -1,37 +1,24 @@
-import React from 'react';
-import { View,Text , StatusBar} from 'react-native';
-import { ScrollView } from 'react-native';
-import Cart from '../Components/Cart';
-import FoodItem from '../Data/FoodItem.json';
-
+import React from "react";
+import { View, Text, StatusBar } from "react-native";
+import { ScrollView } from "react-native";
+import Cart from "../Components/Cart";
+import FoodItem from "../Data/FoodItem.json";
 
 function HomeScreen(props) {
-   
-   
-    const [Data, setData] = React.useState(null);
+  const [Data, setData] = React.useState(null);
 
-    React.useEffect(()=>{
-        
-        
+  React.useEffect(() => {}, [Data]);
+  return (
+    <ScrollView>
+      <StatusBar barStyle={"dark-content"} backgroundColor={"white"} />
 
-    },[Data])
-    return (
-        <ScrollView>
-        <StatusBar barStyle={'dark-content'} backgroundColor={'white'}/>
-
-        {
-            FoodItem && FoodItem.length>0?(
-                FoodItem.map((data,i)=>(
-                    <Cart key={i} data={data}/>
-                
-                ))
-            ):(
-<Text>no data</Text>
-            )
-        }
-           
-        </ScrollView>
-    );
+      {FoodItem && FoodItem.length > 0 ? (
+        FoodItem.map((data, i) => <Cart key={i} data={data} />)
+      ) : (
+        <Text>no data</Text>
+      )}
+    </ScrollView>
+  );
 }
 
 export default HomeScreen;
